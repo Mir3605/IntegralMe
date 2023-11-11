16f183dc77bac1457de6061c7e860b7c1410e78d
Mir3605 <mir3605@gmail.com>
2023-11-11 18:13:14 +0100

    Code cleaning

--- app/src/main/java/com/example/integralmefirst/MainActivity.java
+++ app/src/main/java/com/example/integralmefirst/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.integralmefirst;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,21 +7,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static int difficultyLevelsNumber = 3;
-    private RecyclerView chooseLvlRecView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        chooseLvlRecView = findViewById(R.id.ChooseLvlRecView);
+        RecyclerView chooseLvlRecView = findViewById(R.id.ChooseLvlRecView);
 
         ArrayList<Lvl> lvlsArray = new ArrayList<>();
         for (int i = 0; i < difficultyLevelsNumber; i++) {
@@ -40,17 +36,5 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.BackMenu:
-                Toast.makeText(this, "Back clicked", Toast.LENGTH_SHORT).show();
-                //todo back button activity
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
